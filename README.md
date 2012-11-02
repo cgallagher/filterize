da0ac5105c38b392981e44e7c03a2d11807a1610
Chris Gallagher <cgallagher@gmail.com>
2012-11-02 22:20:32 +0000

    remove some unused code and update readme

--- README.md
+++ README.md
@@ -18,7 +18,10 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+dir = File.dirname(File.expand_path(__FILE__))
+img = File.open("#{dir}/borat.png", 'r')
+overlay_image = File.open("#{dir}/overlay.png", 'r')
+factory = Filterize::Factory.new(img).overlay_image(overlay_image, 100, 100)
 
 ## Contributing
 
--- spec/filterize_spec.rb
+++ spec/filterize_spec.rb
@@ -4,17 +4,7 @@ describe Filterize do
   it "should filter my image" do
     
     dir = File.dirname(File.expand_path(__FILE__))
-    
-    # class FilterizeTest
-    #   include Filterize::Factory
-    # end
-    
-    # # super xmas jumpers
-    # demo_image_url = "https://by-you.s3.amazonaws.com/dev/entries/26/163025_474539090805_2998726_n_original.jpg?1351861043"
-    # 
-    # test = FilterizeTest.new
-    # test.perform(demo_image_url)
-    
+        
     img = File.open("#{dir}/borat.png", 'r')
     overlay_image = File.open("#{dir}/overlay.png", 'r')
     factory = Filterize::Factory.new(img).overlay_image(overlay_image, 100, 100)
