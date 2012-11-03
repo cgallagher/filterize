6f9a45ef91bf2b1d33bbe9c3dfc66eeff33e2546
Chris Gallagher <cgallagher@gmail.com>
2012-11-03 07:44:24 +0000

    remove dependency on railties

--- lib/filterize.rb
+++ lib/filterize.rb
@@ -1,7 +1,7 @@
 require 'active_support'
 require "filterize/version"
 require "filterize/helpers"
-require "filterize/railtie" if defined? Rails
+#require "filterize/railtie" if defined? Rails
 
 module Filterize
 
